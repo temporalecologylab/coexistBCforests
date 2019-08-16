589902b6364f353427f6490f659a9d3cf56e7184
Darwin Sodhi <darwin193@live.com>
2019-08-16 11:36:03 -0700

    New analyzes

---
+++ Analyses/scripts/LicormodelsPSPSFT.R
@@ -0,0 +1,38 @@
+###################################################
+#Models attempting to code dataset to run linear models
+###################################################
+setwd("~/Documents/Ph.D/LI-COR Data (pacific spirit park)/SFT")
+
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+
+library(tidyverse)
+library(dbplyr)
+library(tidyr)
+library(reshape2)
+library(data.table)
+library(tibble)
+library(dplyr)
+
+NMDS_RambspcPSPSFT <- read.csv("~/Documents/Ph.D/LI-COR Data (pacific spirit park)/SFT/NMDS_RambspcPSPSFT.csv", header=FALSE)
+
+colnames(NMDS_RambspcPSPSFT) <- NMDS_RambspcPSPSFT[1,]
+NMDS_RambspcPSPSFT <- NMDS_RambspcPSPSFT[-1,]
+
+colnames(NMDS_RambspcPSPSFT)[1] <- "Species"
+
+
+NMDS_RambspcPSPSFT$specieslatbi<-sub("^([[:alpha:]]*).*", "\\1", NMDS_RambspcPSPSFT$Species)
+
+nmds1 <- melt(NMDS_RambspcPSPSFT, id.vars = c("Species", "specieslatbi"))
+
+path <- unique(nmds1$variable)
+
+for (i in (unique(nmds1$variable))){
+  filename<- paste("Licormodels",i,".csv", sep = "")
+  li <- subset(nmds1, variable == i)
+  lm(value ~ specieslatbi, data= li)
+  s <- summary(lm(value ~ specieslatbi, data= li))
+  s <- as.data.frame(s$coefficients)
+  write.csv(s, filename)
+}
---
+++ Analyses/scripts/LicormodelsPSPcombined.R
@@ -0,0 +1,38 @@
+setwd("~/Documents/Ph.D/LI-COR Data (pacific spirit park)")
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+library(tidyverse)
+library(dbplyr)
+library(tidyr)
+library(reshape2)
+library(data.table)
+library(tibble)
+library(dplyr)
+
+#Import datasets
+NMDS_RambspcPSPSFT <- read.csv("~/Documents/Ph.D/LI-COR Data (pacific spirit park)/SFT/NMDS_RambspcPSPSFT.csv", header=TRUE)
+NMDS_RambspcPSPST <- read.csv("~/Documents/Ph.D/LI-COR Data (pacific spirit park)/NMDS_RambspcPSPST.csv", header=TRUE)
+
+
+colnames(NMDS_RambspcPSPSFT)[1] <- "Species"
+colnames(NMDS_RambspcPSPST)[1] <- "Species"
+
+
+
+NMDS_RambspcPSP<- rbind(NMDS_RambspcPSPSFT,NMDS_RambspcPSPST)
+
+NMDS_RambspcPSP1 <- melt(NMDS_RambspcPSP, id.vars = c("Species", "Site"))
+
+NMDS_RambspcPSP1$specieslatbi<-sub("^([[:alpha:]]*).*", "\\1", NMDS_RambspcPSP1$Species)
+
+path <- unique(NMDS_RambspcPSP1$variable)
+
+for (i in (unique(NMDS_RambspcPSP1$variable))){
+  filename<- paste("LicormodelsPSP",i,".csv", sep = "")
+  li <- subset(NMDS_RambspcPSP1, variable == i)
+  li <- na.omit(li)
+  lm(value ~ Site + specieslatbi, data= li)
+  s <- summary(lm(value ~ Site + specieslatbi, data= li))
+  s <- as.data.frame(s$coefficients)
+  write.csv(s, filename)
+}
