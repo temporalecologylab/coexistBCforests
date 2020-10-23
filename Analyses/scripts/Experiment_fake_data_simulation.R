32adc14c308a7c6d493aebfcdd7d4195105a8a2d
Darwin Sodhi <darwin193@live.com>
2020-10-23 09:31:59 -0700

    new

--- Analyses/scripts/Experiment_fake_data_simulation.R
+++ Analyses/scripts/Experiment_fake_data_simulation.R
@@ -26,27 +26,31 @@ density <- c(0,1)
 temptreat <- c(0,1)
 soil_type <- c(0,1)
 soil_treat <- c(0,1)
+error <- 0.15
 
 
 ### codes for intercept and individual effects
+#y = biomass
 intercepthere <- 50
 soiltype_effect <- -10 #loses 10 units of biomass in conspecific soil
 density_effect <- -5 # as in you LOSE 5 units of biomass in the high treatment
-temp_effect <- -7.5 # as in you LOSE 7.5 units of biomass in the high temp treatment
+temp_effect <- -7.5 # as in you LOSE 7.5 units of biomass in the low temp treatment
 soiltreat_effect <- 5 # as in you Gain 10 units of biomass in the sterilized streatment
+
 
 #### codes for interactions
 densoiltype_intxn <- -12 #high density + conspecific soil causes a loss of 12 units of biomass
-denstemp_intxn <- 2 # as in when you have high density and high temp you GAIN 2 units of biomass
+denstemp_intxn <- 2 # as in when you have high density and low temp you GAIN 2 units of biomass
 densoiltreat_itxn <- -1 #high density + sterilization causes a Loss of one unit of biomass
-tempsoiltype_intxn <- -4 #high temp + conspecific soil causes a loss of 4 units of biomass
-tempsoiltreat_intxn <- -2.5 #high temp + sterilization causes a loss of 2.5 units of biomass
+tempsoiltype_intxn <- -4 #low temp + conspecific soil causes a loss of 4 units of biomass
+tempsoiltreat_intxn <- -2.5 #low temp + sterilization causes a loss of 2.5 units of biomass
 soiltypesoiltreat_intxn <- -1.5 #conspecific soil + sterilization causes a loss of 1.5 unites of biomass
-densoiltypetemp_intxn <- -0.5 #high temp + high density + conspecific soil causes a loss of 0.5 units of biomass
+densoiltypetemp_intxn <- -0.5 #low temp + high density + conspecific soil causes a loss of 0.5 units of biomass
 densoiltreattemp_intxn <- 1 
 densoiltypesoiltreat_intxn <- 2.5
 soiltreattempsoiltype_intxn <- 0.5 
 all_intxn <- -0.75 
+
 
 #codes for number of replicates
 reps_per_treatment <- 10 # 10 is always a good place to start (easy to do the math in your head and check your code)
@@ -72,5 +76,12 @@ df$y_biomass <- intercepthere +
     soiltypesoiltreat_intxn * (df$x_soiltype * df$x_soiltreat) + # 6 2-way intxns
     densoiltypetemp_intxn * (df$x_density * df$x_soiltype * df$x_temp) +
     densoiltreattemp_intxn * (df$x_density * df$x_soiltreat * df$x_temp) +
-    densoiltypesoiltreat_intxn * (df$x_density * df$x_soiltype * df$x_soiltreat) + # 3-way intxns
-    all_intxn * (df$x_density * df$x_temp * df$x_soiltype * df$x_soiltreat) # 1 4-way intxn
+    densoiltypesoiltreat_intxn * (df$x_density * df$x_soiltype * df$x_soiltreat) +
+    soiltreattempsoiltype_intxn * (df$x_soiltreat * df$x_temp * df$x_soiltype ) + #4 3-way intxns
+    all_intxn * (df$x_density * df$x_temp * df$x_soiltype * df$x_soiltreat)  # 1 4-way intxn
+  
+# incorporating 15% error into y
+df$y_pred<- df$y_biomass + rnorm(ntot, error)
+
+#Statistical model to test paramaters
+lm(y_pred~ x_density * x_temp * x_soiltype * x_soiltreat, data= df)
